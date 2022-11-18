39b2c29977d67eaa4ee31f4b621cd8fa0cbf8ca6
Leonardo Candado <candadoleo@gmail.com>
2022-11-18 14:03:52 -0400

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,49 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+data class Usuario(val nome: String, val email: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(var nome: String, val duracao: Int = 60, val nivel: Nivel)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
     }
+
+    fun printUsuarios(){
+        println("Lista de Inscritos")
+        for (usuario in inscritos){
+            println("${usuario.nome} - ${usuario.email}")
+        }
+    }
+
+    fun printMaterias(){
+        println("Lista de Materias")
+        for (materias in conteudos){
+            println("${materias.nome} - ${materias.duracao} - ${materias.nivel} ")
+        }
+    }
+
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+
+    val listaConteudos = mutableListOf<ConteudoEducacional>()
+    listaConteudos.add(ConteudoEducacional("Kotlin - Basico", 40, Nivel.BASICO))
+    listaConteudos.add(ConteudoEducacional("Kotlin - Intermediario", 60, Nivel.INTERMEDIARIO))
+    listaConteudos.add(ConteudoEducacional("Kotlin - Avançado", 120, Nivel.AVANCADO))
+    val DesafioDIO = Formacao("Formação Android Developer", listaConteudos)
+
+    DesafioDIO.matricular(Usuario("Leonardo", "leonardo@gmail.com"))
+    DesafioDIO.matricular(Usuario("Pedro", "pedro@gmail.com"))
+    DesafioDIO.matricular(Usuario("Guilherme", "guilherme@gmail.com"))
+
+
+    DesafioDIO.printMaterias()
+    println("-------------------------")
+    DesafioDIO.printUsuarios()
 }
